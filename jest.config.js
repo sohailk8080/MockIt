c48099d4b1a83c00dfbcced2615007fdf0e44e1f
Sohail Khan <sohail@Sohails-MacBook-Pro.local>
2025-06-15 02:52:38 +0530

    adding reporter

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
+  reporters: [
+    "default",
+    ["jest-html-reporter", {
+      outputPath: "reports/test-report.html"
+    }]
+  ],
   testMatch: [
     '**/pact/**/*.test.js',
     '**/validation/**/*.test.js'
